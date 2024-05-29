15034cd2aae9790c3d11fcc4d7c80a4740722246
Yytsi <tuukka.yildirim@gmail.com>
2024-05-29 17:30:23 +0300

    fix(removed useless createfile test script)

--- server/rss-fetcher/createfile.py
+++
@@ -1,7 +0,0 @@
-# get the number of files in this directory
-import os
-
-num_of_files = len([name for name in os.listdir('.') if os.path.isfile(name)])
-
-with open(f"file{num_of_files}.txt", "w") as f:
-    f.write(f"Hello, {num_of_files*num_of_files}\n")
