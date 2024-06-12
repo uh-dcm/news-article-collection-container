8f6079efe32da0bdabb1f54b97864dcdb7fdfbc7
Miska Pajukangas <56785774+mpjk@users.noreply.github.com>
2024-06-12 21:55:00 +0300

    feat(ui): add accordion menu, change toast styles
    Issue: #14

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,12 @@ import { Button } from '@/components/ui/button';
 import { toast } from 'sonner';
 import { Textarea } from '@/components/ui/textarea';
 import { Label } from '@/components/ui/label';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const serverUrl = import.meta.env.PROD
@@ -23,6 +29,7 @@ export const serverUrl = import.meta.env.PROD
 function App() {
   const [feedUrls, setFeedUrls] = useState('');
   const [isDisabled, setIsDisabled] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
 
   const handleInputChange = (event: {
     target: { value: React.SetStateAction<string> };
@@ -31,27 +38,38 @@ function App() {
   };
 
   const handleSubmit = async () => {
+    toast.dismiss();
     const rssFeeds = feedUrls
       .split('\n')
       .map((url) => url.trim())
       .filter((url) => url !== '');
     console.log(rssFeeds);
     if ((await sendAllFeedUrls(rssFeeds)).status == 200) {
-      toast.success('URLs Set Successfully!');
+      toast.success('Feed list set successfully!');
     }
   };
 
   const handleFetchStart = () => {
-    toast.info('URL fetching started...');
+    toast.loading('RSS fetching in progress...', {
+      style: {
+        background: 'hsl(208, 100%, 97%)',
+        border: '1px solid hsl(221, 91%, 91%)',
+        color: 'hsl(210, 92%, 45%)',
+      },
+    });
+    setIsFetching(true);
     keepFetching();
   };
 
   const handleFetchStop = () => {
-    toast.info('URL fetching stopped.');
+    setIsFetching(false);
+    toast.dismiss();
+    toast.warning('RSS fetching stopped.');
     stopFetching();
   };
 
   const handleArticleDownload = async () => {
+    toast.dismiss();
     setIsDisabled(true);
 
     toast.promise(
@@ -100,7 +118,7 @@ function App() {
 
   return (
     <div className="flex min-h-[100vh] flex-col">
-      <div className="border-border/70 relative flex items-center border-b p-3">
+      <div className="border-border/70 relative flex items-center border-b p-3 shadow-sm shadow-slate-50">
         <img
           className="mx-2"
           width="40"
@@ -129,11 +147,11 @@ function App() {
         </div>
       </div>
       <div className="mb-20 flex justify-center">
-        <div className="grid w-[550px] grid-cols-2 grid-rows-2 gap-4">
+        <div className="grid w-[550px] grid-cols-2 grid-rows-3 gap-4">
           <Button
             variant="outline"
             onClick={handleSubmit}
-            className="col-span-2 p-5 text-base"
+            className="col-span-2 p-6 text-base"
           >
             <div className="flex justify-center">
               <CheckIcon className="mr-3 size-6"></CheckIcon>
@@ -141,9 +159,10 @@ function App() {
             </div>
           </Button>
           <Button
-            className="p-5 text-base"
+            className="p-6 text-base"
             variant="outline"
             onClick={handleFetchStart}
+            disabled={isFetching}
           >
             <div className="flex justify-center">
               <BarsArrowUpIcon className="mr-3 size-6"></BarsArrowUpIcon>
@@ -151,7 +170,7 @@ function App() {
             </div>
           </Button>
           <Button
-            className="p-5 text-base"
+            className="p-6 text-base"
             variant="outline"
             onClick={handleFetchStop}
           >
@@ -164,7 +183,7 @@ function App() {
             variant="outline"
             onClick={handleArticleDownload}
             disabled={isDisabled}
-            className="col-span-2 p-5 text-base"
+            className="col-span-2 p-6 text-base"
           >
             {isDisabled ? (
               'Downloading...'
@@ -180,13 +199,52 @@ function App() {
               Please note that the download might take some time.
             </p>
           )}
+
+          <div className="col-span-2 mt-16">
+            <h2 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0">
+              Q&A
+            </h2>
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1">
+                <AccordionTrigger>
+                  How does the collector work?
+                </AccordionTrigger>
+                <AccordionContent>
+                  The collector gathers all the articles from the RSS-feed
+                  addresses you have provided. When you decide to download the
+                  content, the program parses all the articles and it then forms
+                  a{' '}
+                  <a
+                    className="opacity-50 hover:underline hover:opacity-100"
+                    href="https://en.wikipedia.org/wiki/JSON"
+                  >
+                    JSON-file
+                  </a>{' '}
+                  from the information it gathered.
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-2">
+                <AccordionTrigger>How do I use it?</AccordionTrigger>
+                <AccordionContent>
+                  The process is quite simple:
+                  <ul className="mt-2 list-inside list-decimal space-y-2">
+                    <li>Input the wanted RSS-feeds in the textarea</li>
+                    <li>Activate the RSS-fetching</li>
+                    <li>Disable the fetching when needed</li>
+                    <li>Download the gathered articles</li>
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
         </div>
       </div>
+
       <div className="mt-auto flex w-full flex-col items-center p-3">
         <div className="mb-3 flex items-center">
           <Label>
             <a
-              className="hover:underline"
+              className="underline-offset-2 hover:underline"
               href="https://github.com/uh-dcm/news-article-collection-container"
             >
               &copy; University of Helsinki, Digital and Computational Methods
