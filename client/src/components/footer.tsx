8e95122516a68078f9d32e769451037695bcd824
Miska Pajukangas <56785774+mpjk@users.noreply.github.com>
2024-06-14 18:09:52 +0300

    feat(ui): add github link to header, refactor
    Issue: #14

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -5,12 +5,7 @@ export default function Footer() {
     <div className="mt-auto flex w-full flex-col items-center p-3">
       <div className="mb-3 flex items-center">
         <Label>
-          <a
-            className="underline-offset-2 hover:underline"
-            href="https://github.com/uh-dcm/news-article-collection-container"
-          >
-            &copy; University of Helsinki, Digital and Computational Methods
-          </a>
+          &copy; University of Helsinki, Digital and Computational Methods
         </Label>
       </div>
     </div>
