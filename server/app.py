8524d24ff1940a5cbde1b8644108f9bffe810fc2
Yytsi <tuukka.yildirim@gmail.com>
2024-06-17 10:40:55 +0300

    feat(process.py populates the db every 6 minutes)

--- server/app.py
+++ server/app.py
@@ -22,11 +22,15 @@ scheduler = BackgroundScheduler()
 def run_collect_script():
     subprocess.run(["python", "collect.py"], cwd='./rss-fetcher') # setting script to happen in rss-fetcher folder
 
+def run_process_script():
+    subprocess.run(["python", "process.py"], cwd='./rss-fetcher') # setting script to happen in rss-fetcher folder
+
 
 @app.route('/api/start', methods=['POST'])
 def start_fetching():
     if not scheduler.get_jobs():
         scheduler.add_job(run_collect_script, 'interval', minutes=5, id='fetcher-collect', next_run_time=datetime.now())
+        scheduler.add_job(run_process_script, 'interval', minutes=6, id='fetcher-process')
         scheduler.start()
         return jsonify({"status": "started"}), 200
     return jsonify({"status": "already running"}), 200
@@ -35,6 +39,7 @@ def start_fetching():
 def stop_fetching():
     if scheduler.get_jobs():
         scheduler.remove_job('fetcher-collect')
+        scheduler.remove_job('fetcher-process')
         return jsonify({"status": "stopped"}), 200
     return jsonify({"status": "it was not running"}), 200
 
