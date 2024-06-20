f68c17a6a2d38f7591b41f10285ec5776338d754
Miska Pajukangas <56785774+mpjk@users.noreply.github.com>
2024-06-20 19:56:33 +0300

    chore(ui): update q&a text

--- client/src/components/questions-accordion.tsx
+++ client/src/components/questions-accordion.tsx
@@ -16,8 +16,8 @@ export default function QuestionsAccordion() {
           <AccordionTrigger>How does the collector work?</AccordionTrigger>
           <AccordionContent>
             The collector gathers all the articles from the RSS-feed addresses
-            you have provided. When you decide to download the content, the
-            program parses all the articles and it then forms a{' '}
+            you have provided and confirmed. When you decide to download the
+            content, the program parses all the articles and it then forms a{' '}
             <a
               className="opacity-50 hover:underline hover:opacity-100"
               href="https://en.wikipedia.org/wiki/JSON"
@@ -32,10 +32,22 @@ export default function QuestionsAccordion() {
           <AccordionContent>
             The process is quite simple:
             <ul className="mt-2 list-inside list-decimal space-y-2">
-              <li>Input the wanted RSS-feeds in the textarea</li>
+              <li>Input the wanted RSS-feeds one-by-one</li>
+              <li>
+                When the you have the all the feeds you wanted, confirm the
+                feeds by clicking 'Send selected RSS feeds'
+              </li>
+              <li>
+                (Optional): If you wish, you can select and delete RSS-feeds,
+                remember to finalize the changes by confirming, just as done
+                before.
+              </li>
               <li>Activate the RSS-fetching</li>
               <li>Disable the fetching when needed</li>
-              <li>Download the gathered articles</li>
+              <li>
+                Download the gathered articles or preview them by querying with
+                search
+              </li>
             </ul>
           </AccordionContent>
         </AccordionItem>
