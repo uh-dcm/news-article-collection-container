91eaa6ab148bf98f654553324229e76b68794555
tihyyti <timo.hyyppa@helsinki.fi>
2024-06-25 16:45:54 +0300

    Fixed UI-text from json to csv.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -192,7 +192,7 @@ export default function App() {
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement('a');
         link.href = url;
-        link.setAttribute('download', 'articles.json');
+        link.setAttribute('download', 'articles.csv');
         document.body.appendChild(link);
         link.click();
 
