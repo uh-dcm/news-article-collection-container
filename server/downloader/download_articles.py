5254e23c351e0fa914a80ab324ee8ea0bb563658
ayriainen <lounastarjous@gmail.com>
2024-06-30 15:18:02 +0300

    refactor(backend): reduce downloader copypaste

--- server/downloader/download_articles.py
+++ server/downloader/download_articles.py
@@ -5,41 +5,53 @@ import os
 import time
 from flask import jsonify, send_from_directory, request
 from sqlalchemy import inspect
+from sqlalchemy.exc import SQLAlchemyError
 from config import FETCHER_FOLDER
 from log_config import logger
-from downloader.db_to_json import transform_articles_to_json
-from downloader.db_to_csv import transform_articles_to_csv
-from downloader.db_to_parquet import transform_articles_to_parquet
+from downloader.db_to_format_transformer import transform_db_to_format
 
 LOCK_FILE = f'./{FETCHER_FOLDER}/processing.lock'
 
 def transform_articles(format):
-    if format == 'json':
-        transform_articles_to_json()
-        return send_from_directory(f'./{FETCHER_FOLDER}/data', "articles.json", as_attachment=True)
-    elif format == 'csv':
-        transform_articles_to_csv()
-        return send_from_directory(f'./{FETCHER_FOLDER}/data', "articles.csv", as_attachment=True)
-    elif format == 'parquet':
-        transform_articles_to_parquet()
-        return send_from_directory(f'./{FETCHER_FOLDER}/data', "articles.parquet", as_attachment=True)
-    else:
-        return jsonify({"status": "error", "message": "Invalid format requested."}), 400
+    output_file_path = None
+    try:
+        transform_db_to_format(format)
+        if format == 'json':
+            output_file_path = "articles.json"
+        elif format == 'csv':
+            output_file_path = "articles.csv"
+        elif format == 'parquet':
+            output_file_path = "articles.parquet"
+        else:
+            return jsonify({"status": "error", "message": "Invalid format requested."}), 400
+
+        return send_from_directory(f'./{FETCHER_FOLDER}/data', output_file_path, as_attachment=True)
+    except Exception as e:
+        logger.error(f"Running process and export resulted in failure: {e}")
+        return jsonify({"status": "error", "message": str(e)}), 400
 
 def download_articles(engine):
+    # '/api/articles' defaults to json for backwards compatibility for now
     format = request.args.get('format', 'json')
 
     # wait for collect.py and process.py to finish
     while os.path.exists(LOCK_FILE):
         time.sleep(1)
 
-    # check whether the table exists
-    inspector = inspect(engine)
-    if not inspector.has_table('articles'):
-        return jsonify({"status": "error", "message": "No articles found. Please fetch the articles first."}), 400
-
     try:
+        # check whether the table exists, this is used often
+        inspector = inspect(engine)
+        if not inspector.has_table('articles'):
+            return jsonify({"status": "error", "message": "No articles found. Please fetch the articles first."}), 400
+
+        # this format check is just extra security, likely never used
+        if format not in ['json', 'csv', 'parquet']:
+            return jsonify({"status": "error", "message": "Invalid format requested."}), 400
+
         return transform_articles(format)
+    except SQLAlchemyError as e:
+        logger.error(f"Database error: {e}")
+        return jsonify({"status": "error", "message": f"Database error: {str(e)}"}), 500
     except Exception as e:
         logger.error(f"Running process and export resulted in failure: {e}")
-        return jsonify({"status": f"{e}"}), 400
+        return jsonify({"status": "error", "message": str(e)}), 400
---
+++ server/tests/test_db_to_format_transformer.py
@@ -0,0 +1,87 @@
+"""
+This has tests for the transformations of db_to_format_transformer.py.
+"""
+import os
+import sys
+import json
+import csv
+import shutil
+import pytest
+import pandas as pd
+from copy import deepcopy
+from database_filler import fill_test_database
+
+# path needs to be before db_to_format_transformer import, at least in local tests
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from downloader.db_to_format_transformer import db_connect, transform_db_to_format  # pylint: disable=import-error
+
+@pytest.fixture(scope='module')
+def setup_database():
+    conn = db_connect.connect()
+    fill_test_database(conn)
+    yield conn
+    conn.close()
+
+@pytest.fixture(scope='function')
+def setup_and_teardown():
+    os.makedirs('test-rss-fetcher/data', exist_ok=True)
+    yield
+    shutil.rmtree('test-rss-fetcher')
+
+expected_data = [
+    {
+        'id': 1,
+        'url': 'https://blabla.com/article1',
+        'html': '&lt;!DOCTYPE html&gt;&lt;html lang=&quot;fi&quot;&gt;&lt;head&gt;',
+        'full_text': 'Full text 1.',
+        'time': '2016-06-06 09:09:09',
+        'download_time': '2024-04-04 08:08:08.777777'
+    },
+    {
+        'id': 2,
+        'url': 'https://blabla.com/article2',
+        'html': '&lt;p&gt;Html 2&lt;/p&gt;',
+        'full_text': 'Full text 2.',
+        'time': '2016-06-06 09:09:09',
+        'download_time': '2024-04-04 08:08:08.777777'
+    }
+]
+
+def verify_csv_data(file_path, expected_data):
+    # csv has the id as string, not int, so expected_data needs conversion
+    expected_csv_data = deepcopy(expected_data)
+    for item in expected_csv_data:
+        item['id'] = str(item['id'])
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        csv_data = list(reader)
+    assert csv_data == expected_csv_data, f"The data in articles.csv does not match the expected data. Expected: {expected_csv_data}, Actual: {csv_data}"
+
+def verify_json_data(file_path, expected_data):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+    assert json_data == expected_data, f"The data in articles.json does not match the expected data. Expected: {expected_data}, Actual: {json_data}"
+
+def verify_parquet_data(file_path, expected_data):
+    df = pd.read_parquet(file_path)
+    parquet_data = df.to_dict(orient='records')
+    assert parquet_data == expected_data, f"The data in articles.parquet does not match the expected data. Expected: {expected_data}, Actual: {parquet_data}"
+
+def test_transform_articles_to_csv(setup_database, setup_and_teardown):
+    transform_db_to_format('csv')
+    file_path = 'test-rss-fetcher/data/articles.csv'
+    assert os.path.exists(file_path), "The articles.csv file was not created."
+    verify_csv_data(file_path, expected_data)
+
+def test_transform_articles_to_json(setup_database, setup_and_teardown):
+    transform_db_to_format('json')
+    file_path = 'test-rss-fetcher/data/articles.json'
+    assert os.path.exists(file_path), "The articles.json file was not created."
+    verify_json_data(file_path, expected_data)
+
+def test_transform_articles_to_parquet(setup_database, setup_and_teardown):
+    transform_db_to_format('parquet')
+    file_path = 'test-rss-fetcher/data/articles.parquet'
+    assert os.path.exists(file_path), "The articles.parquet file was not created."
+    verify_parquet_data(file_path, expected_data)
