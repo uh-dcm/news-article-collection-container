7ebfea6828712c743f8f82426f63a8d536e72073
ayriainen <lounastarjous@gmail.com>
2024-07-03 02:35:52 +0300

    fix: paste the function into newest app.py

--- server/app.py
+++ server/app.py
@@ -3,13 +3,18 @@ This is the main backend app for the project.
 """
 from os.path import exists
 import os
-import threading
 import subprocess
-import time
 from flask import Flask, jsonify, send_from_directory, request
 from flask_cors import CORS
-from sqlalchemy import create_engine, MetaData, text, inspect
+from flask_apscheduler import APScheduler
+from sqlalchemy import create_engine, MetaData, inspect, text
 from config import DATABASE_URL, FETCHER_FOLDER
+from log_config import logger, LOG_FILE_PATH
+from services.download import download_articles
+from services.search import search_articles
+
+class Config:
+    SCHEDULER_API_ENABLED = True
 
 os.makedirs(f"./{FETCHER_FOLDER}/data/", exist_ok=True)
 engine = create_engine(DATABASE_URL, echo=False)
@@ -18,57 +23,64 @@ connection = engine.connect()
 
 app = Flask(__name__, static_folder='static')
 CORS(app, resources={r"/*": {"origins": "*"}})
+app.config.from_object(Config())
 
-SCHEDULER_RUNNING = False
-SCHEDULER_THREAD = None
-PROCESSING_ACTIVE = False
-STOP_EVENT = threading.Event()
+scheduler = APScheduler()
+scheduler.init_app(app)
 
-def run_collect_and_process_script():
-    global SCHEDULER_THREAD, SCHEDULER_RUNNING, PROCESSING_ACTIVE
+LOCK_FILE = f'./{FETCHER_FOLDER}/processing.lock'
 
-    while SCHEDULER_RUNNING:
-        try:
-            PROCESSING_ACTIVE = True
-            subprocess.run(['python', 'collect.py'], cwd=f'./{FETCHER_FOLDER}', check=True)
-            subprocess.run(['python', 'process.py'], cwd=f'./{FETCHER_FOLDER}', check=True)
-            PROCESSING_ACTIVE = False
-        except subprocess.CalledProcessError as e:
-            print("Error: ", e.stderr)
-            PROCESSING_ACTIVE = False
-        
-        # this process will repeat, and this is to make it 5 mins
-        # the stop_event is to make the tests not wait
-        STOP_EVENT.wait(300)
+# this is triggered by start_fetching
+# runs collect.py and process.py on the submitted feeds
+# hogs database for itself with the lock
+def run_collect_and_process():
+    if os.path.exists(LOCK_FILE):
+        print("Processing is already active.")
+        return
+
+    try:
+        with open(LOCK_FILE, 'w') as f:
+            f.write('processing')
+
+        result = subprocess.run(['python3', 'collect.py'], cwd=f'./{FETCHER_FOLDER}', capture_output=True, check=True, text=True)
+        if result.stdout:
+            logger.info(result.stdout.strip())
+        result = subprocess.run(['python3', 'process.py'], cwd=f'./{FETCHER_FOLDER}', capture_output=True, check=True, text=True)
+        if result.stdout:
+            logger.info(result.stdout.strip())
+
+    except subprocess.CalledProcessError as e:
+        print("Error: ", e.stderr)
+    finally:
+        if os.path.exists(LOCK_FILE):
+            os.remove(LOCK_FILE)
 
 @app.route('/api/start', methods=['POST'])
 def start_fetching():
-    global SCHEDULER_THREAD, SCHEDULER_RUNNING
-
-    if not SCHEDULER_RUNNING:
-        SCHEDULER_RUNNING = True
-        SCHEDULER_THREAD = threading.Thread(target=run_collect_and_process_script)
-        SCHEDULER_THREAD.start()
+    if not scheduler.get_job('collect_and_process'):
+        scheduler.add_job(
+            id='collect_and_process',
+            func=run_collect_and_process,
+            trigger='interval',
+            minutes=5,
+            misfire_grace_time=300
+        )
+        run_collect_and_process()
         return jsonify({"status": "started"}), 201
     else:
         return jsonify({"status": "already running"}), 409
 
 @app.route('/api/stop', methods=['POST'])
 def stop_fetching():
-    global SCHEDULER_THREAD, SCHEDULER_RUNNING
-
-    if SCHEDULER_RUNNING:
-        SCHEDULER_RUNNING = False
-        SCHEDULER_THREAD = None
+    if scheduler.get_job('collect_and_process'):
+        scheduler.remove_job('collect_and_process')
         return jsonify({"status": "stopped"}), 200
     else:
         return jsonify({"status": "it was not running"}), 409
 
 @app.route('/api/status', methods=['GET'])
 def fetching_status():
-    global SCHEDULER_RUNNING
-
-    if SCHEDULER_RUNNING:
+    if scheduler.get_job('collect_and_process'):
         return jsonify({"status": "running"}), 200
     else:
         return jsonify({"status": "stopped"}), 400
@@ -100,38 +112,15 @@ def serve(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
+# downloads the articles from db, uses download_articles.py
 @app.route('/api/articles', methods=['GET'])
-def download_articles():
-    global PROCESSING_ACTIVE
+def download():
+    return download_articles(engine)
 
-    while PROCESSING_ACTIVE:
-        time.sleep(1)
-
-    inspector = inspect(engine)
-    if not inspector.has_table('articles'):
-        return jsonify({"status": "error", "message": "No articles found. Please fetch the articles first."}), 400
-
-    try:
-        subprocess.run(['python', 'db_to_json.py'], check=True)
-        return send_from_directory(f'./{FETCHER_FOLDER}/data', "articles.json", as_attachment=True)
-    except subprocess.CalledProcessError as e:
-        print("Running process and export resulted in failure")
-        print("Error: ", e.stderr)
-        return jsonify({"status": f"{e}"}), 400
-
+# search db for a query and return results, uses search_articles.py
 @app.route('/api/articles/search', methods=['GET'])
-def search_articles():
-    try:
-        search_query = request.args.get('searchQuery', '')
-        stmt = text("SELECT STRFTIME('%d/%m/%Y, %H:%M', time), url, full_text FROM articles WHERE full_text LIKE :word COLLATE utf8_general_ci")
-        stmt = stmt.bindparams(word=f'%{search_query}%')
-        result = connection.execute(stmt)
-        rows = result.fetchall()
-        data = [{"time": time, "url": url, "full_text": full_text} for time, url, full_text in rows]
-        return jsonify(data), 200
-    except Exception as e:
-        print("Error: ", e)
-        return jsonify({"status": "error", "message": str(e)}), 500
+def search():
+    return search_articles(engine)
 
 @app.route('/api/articles/statistics', methods=['GET'])
 def aggregate_by_domain():
@@ -154,5 +143,15 @@ def aggregate_by_domain():
         print("Error: ", e)
         return jsonify({"status": "error", "message": str(e)}), 500
 
+@app.route('/api/error_logs', methods=['GET'])
+def get_error_log():
+    try:
+        with open(LOG_FILE_PATH, 'r') as log_file:
+            log_records = log_file.read()
+        return jsonify(logs=log_records.splitlines()), 200
+    except Exception as e:
+        return jsonify({"error": "Failed to fetch logs"}), 500
+
 if __name__ == '__main__':
+    scheduler.start()
     app.run(host='0.0.0.0', port=5000)
