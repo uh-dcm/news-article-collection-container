74359cc92024604ba7c6170dd5dbf5ebaa0560a5
parissak <sapak@protonmail.com>
2024-07-02 16:34:45 +0300

    chore: fix for test

--- client/src/services/log_records.tsx
+++ client/src/services/log_records.tsx
@@ -3,7 +3,7 @@ import { serverUrl } from '../App';
 
 const getLogRecords = async () => {
   const response = await axios.get(`${serverUrl}/api/error_logs`);
-  return response.data;
+  return response.data.logs;
 };
 
 export { getLogRecords };
