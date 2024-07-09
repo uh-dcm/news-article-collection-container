d920489c8417fe5b55c96a6d9daedc3ed25c6959
immone <antti.imm1n@gmail.com>
2024-07-10 01:04:41 +0300

    fix/feat: change time series lib

--- client/src/components/timeseries.tsx
+++ client/src/components/timeseries.tsx
@@ -1,83 +1,65 @@
-import { ResponsiveContainer,
-Tooltip,
-TooltipProps,
-LineChart,
-XAxis,
-YAxis,
-Label,
-Line 
-} from 'recharts';
+import ReactApexChart from 'react-apexcharts';
+import { ApexOptions } from 'apexcharts';
 
-import { timeFormat } from "d3-time-format";
-import {
-    ValueType,
-    NameType,
-} from 'recharts/types/component/DefaultTooltipContent';
-
-const CustomTooltip = ({
-    active,
-    payload,
-    label,
-}: TooltipProps<ValueType, NameType>) => {
-    if (active) {
-    return (
-        <div className="custom-tooltip">
-        <p className="label">{`${new Date(label).toLocaleDateString()} : ${payload?.[0].value  }`}</p>
-        </div>
-    );
+const options: ApexOptions = {
+  chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: true
+    },
+    animations: {
+      enabled: true
     }
-
-    return null;
+  },
+  xaxis: {
+    type: 'datetime'
+  },
+  stroke: {
+    curve: 'stepline',
+  },
+  markers: {
+    size: 2,
+  },
+  tooltip: {
+    enabled: true,
+    theme: "dark",
+    followCursor: true
+  },
+  noData: {
+    text: "No data to show."
+  }
 };
 
 type DomainData = {
-    name: string
-    count: number
+  name: string
+  count: number
 };
-  
 
-export default function TimeSeries({data}: {data: DomainData[] })  {
-    return ( 
-    <div className="mt-3 h-[300px]">
-    <ResponsiveContainer width={"100%"} height={300}>
-    <LineChart
-      data={data === undefined
-                    ? [ {time: 0, count: 0} ]
-                    :
-                    data.map(i => {
-                        return {
-                        time: new Date(i.name.replace(/\s/g, 'T').concat("Z")).getTime(),
-                        count: i.count
-                        }
-                    })
-            }
-      margin={{ top: 30, right: 30, left: 30, bottom: 30 }}
-    >
-      <XAxis type="number" dataKey="time" scale="time" domain={["dataMin", "dataMax"]} tickFormatter={timeFormat("%d-%m%")}>
-        <Label
-          value={"Time (date)"}
-          position="bottom"
-          style={{ textAnchor: "middle" }}
-        />
-      </XAxis>
-      <YAxis>
-        <Label
-          value={"Count (number of articles)"}
-          position="left"
-          angle={-90}
-          style={{ textAnchor: "middle" }}
-        />
-      </YAxis>
-      <Tooltip content={<CustomTooltip />} cursor={{ fill: "transparent" }} />
-      <Line
-        dataKey="count"
-        name="number of articles"
-        unit={(" ")}
-        dot={false}
-        type={"natural"}
-      />
-    </LineChart>
-  </ResponsiveContainer>
-    </div>
+
+const TimeSeries = ({data}: {data: DomainData[]}) => {
+
+  const series = 
+    [{
+    name: "Count",
+    data: data === undefined 
+    ? [] 
+    :  
+    data.map(i => {
+    return {
+    x: new Date(i.name.replace(/\s/g, 'T').concat("Z")),
+    y: i.count
+    }
+})
+}]
+  return (
+    <ReactApexChart
+      options={options}
+      series={series}
+      type="line"
+      height={350}
+  />
     )
 }
+
+export default TimeSeries 
