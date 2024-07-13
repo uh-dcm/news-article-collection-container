35b326c64b606fbf8a8b77e1bba85c09b06e3f93
ayriainen <150022279+ayriainen@users.noreply.github.com>
2024-07-13 03:19:34 +0300

    docs: add updated runtests.sh for ease of testing, update related docs

---
+++ runtests.sh
@@ -0,0 +1,54 @@
+# Place at project root, run there in terminal with: bash runtests.sh
+
+# Things that may help in rare non-code related issues:
+# 1. npm depency discrepancy issue: 
+# delete node_modules folder, let it get reinstalled by this script fresh
+# 2. docker build not starting:
+# restart your system's Docker (Docker Desktop etc.)
+# run "docker system prune" to free up resources
+# 3. terminal window affected by something: 
+# close the terminal window and open a fresh one
+
+run_pytests() {
+    echo "Setting up venv for Pytests"
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r server/requirements.txt
+    echo "Running Pytests"
+    cd server
+    pytest
+    echo "Pytests done"
+    deactivate
+    cd ..
+}
+
+run_vitests() {
+    echo "Running Vitests"
+    cd client
+    npm install
+    npm test
+    echo "Vitests done"
+}
+
+run_cypress_tests() {
+    echo "Starting container build"
+    docker compose up --build -d
+    echo "Running Cypress tests"
+    npx cypress run
+    echo "Cypress tests done"
+    cd ..
+    echo "Stopping container"
+    docker compose down
+}
+
+echo "Starting tests"
+
+run_pytests
+sleep 1
+
+run_vitests
+sleep 1
+
+run_cypress_tests
+
+echo "Tests ended"
